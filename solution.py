a47c38813187166c0bcd57780a6f1024a59fd29b
jgohl-dci <johannes.gohl@dci-student.org>
2023-08-30 14:34:06 +0200

    first commit

---
+++ solution.py
@@ -0,0 +1,24 @@
+# T1
+city = 'London'
+print(city)
+
+
+# T2
+city = 'berlin'
+population = 645000
+
+print(city.capitalize(), ': ',population, sep='')
+
+
+# T3
+city = 'london'
+population = 9000000
+
+print('City: ', city.capitalize(), type(city) == str)
+print('Population:', population)
+
+
+# T4
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+print(len(text))
